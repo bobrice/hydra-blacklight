93b6b0dee5fd91c645575ecfb6805b97b542f616
ksprague25 <kalee.sprague@yale.edu>
2013-04-26 15:55:45 -0400

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiggitHydra::Application.config.secret_token = 'cfee9e209d8ce818e7adf75a97b66986e988c26ac4fbabcc2535ed68bd630bc54e7a0bf9ba4508a2eb0100cee799c1c51381a78c4a0fd69aae17f17c0c405aa6'
