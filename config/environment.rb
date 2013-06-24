9e1ed0b16fc5fdd8e4551dad97b302b42ad073aa
Eric James <eric.james@yale.edu>
2013-06-24 15:17:38 -0400

    add digital collection to model, and set up dev and test in hydra ymls

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 DiggitHydra::Application.initialize!
 
 CASClient::Frameworks::Rails::Filter.configure(
-  :cas_base_url => "https://secure.its.yale.edu/cas/",
+  :cas_base_url => "https://securedev.its.yale.edu/cas/",
   :username_session_key => :cas_user,
   :extra_attributes_session_key => :cas_extra_attributes
 )
