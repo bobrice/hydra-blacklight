8fc650a07f5f78b0b9dc1c9ecd3a5828a8fdbb44
Lakeisha Robinson <lakeisha.robinson@yale.edu>
2013-08-30 15:59:10 -0400

    Suppressed dropdown options

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -96,10 +96,10 @@ class CatalogController < ApplicationController
     config.add_show_field 'notes_tsim', :label => 'Notes:'
     config.add_show_field 'abstract_tsim', :label => 'Abstract:'
     config.add_show_field 'variant_titles_tsim', :label => 'Variant Titles:' 
-    config.add_show_field 'assoc_names_tsim', :label => 'Associated Names:' 
-    config.add_show_field 'subject_geographic_tsim', :label => 'Geographic Subjects:'
+    config.add_show_field 'subject_name_tsim', :label => 'Subjects:'
     config.add_show_field 'subject_topic_tsim', :label => 'Subjects:'
-    config.add_show_field 'local_subject_tsim', :label => 'Local Subjects:'
+    config.add_show_field 'subject_geographic_tsim', :label => 'Subjects:'
+    config.add_show_field 'local_subject_tsim', :label => 'Subjects:'
     config.add_show_field 'event_tsim', :label => 'Events:'
     config.add_show_field 'period_style_tsim', :label => 'Period/Style:'
     config.add_show_field 'culture_tsim', :label => 'Culture:'
@@ -159,43 +159,45 @@ class CatalogController < ApplicationController
     config.add_search_field 'all_fields', :label => 'All Fields'
     
 
-    # Now we see how to over-ride Solr request handler defaults, in this
+    #    # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields. 
     
-    config.add_search_field('title') do |field|
+    #config.add_search_field('title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params. 
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+    #  field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = { 
-        :qf => '$title_qf',
-        :pf => '$title_pf'
-      }
-    end
+
+      #field.solr_local_parameters = { 
+      #  :qf => '$title_qf',
+      #  :pf => '$title_pf'
+      #}
+    #end
     
-    config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-      field.solr_local_parameters = { 
-        :qf => '$author_qf',
-        :pf => '$author_pf'
-      }
-    end
+    #config.add_search_field('author') do |field|
+
+    #  field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+    #  field.solr_local_parameters = { 
+    #    :qf => '$author_qf',
+    #    :pf => '$author_pf'
+    #  }
+    #end
     
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as 
     # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
-    config.add_search_field('subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
-      field.qt = 'search'
-      field.solr_local_parameters = { 
-        :qf => '$subject_qf',
-        :pf => '$subject_pf'
-      }
-    end
+    #config.add_search_field('subject') do |field|
+    #  field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+    #  field.qt = 'search'
+    #  field.solr_local_parameters = { 
+    #    :qf => '$subject_qf',
+    #    :pf => '$subject_pf'
+    #  }
+    #end
 
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
